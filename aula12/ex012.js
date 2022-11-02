55567cda05a85fd4df953c994dc962e4648d50d6
Andre Carmona <andre.carmona@ymail.com>
2022-11-02 10:49:50 -0300

    cmt

---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+var hora = 2
+if(hora < 12 && hora >= 5) {
+    console.log(`Agora são exatamente ${hora} horas da manhã`)
+    console.log('Bom Dia')
+} else if(hora <= 18 && hora > 11) {
+    console.log(`Agora são exatamente ${hora} horas da tarde`)
+    console.log('Boa Tarde')
+} else if(hora >= 19 && hora <= 23) {
+    console.log(`Agora são exatamente ${hora} horas da noite`)
+    console.log('Boa Noite')
+} else {
+    console.log(`Agora são exatamente ${hora} horas da madrugada`)
+    console.log('Boa Madrugada')
+}
