015def042fdeaa4a2d7d9bbffcc95dbf4d3c322d
Andre Carmona <andre.carmona@ymail.com>
2022-11-02 13:32:43 -0300

    Mudando img automaticamente conforme a hora do dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,15 +2,19 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 23
     msg.innerHTML = `Agora são ${hora} horas`
 
     if(hora >= 0 && hora < 12) {
         img.src = 'foto-manha.png'
+        document.body.style.background = '#b1a832'
     } else if(hora >= 12 && hora < 18) {
         img.src = 'foto-tarde.png'
+        document.body.style.background = '#f49145'
     } else {
         img.src = 'foto-noite.png'
+        document.body.style.background = '#3f3f3f'
     }
 }
 
